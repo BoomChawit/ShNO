f5ec6638227a6c8e517011e25164744e9c1bbb03
BoomChawit <boomchawit.2203@gmail.com>
2023-12-21 22:50:53 +0900

    add ShFNO model

--- ShFNO.py
+++ ShFNO.py
@@ -51,3 +51,132 @@ y_dataset = torch.cat([displacement_dataset, acceleration_dataset], axis = 1)
 
 x_dataset = x_dataset.permute(0, 2, 1)
 y_dataset = y_dataset.permute(0, 2, 1)
+
+ntrain = 600
+
+x_trainset, x_testset = x_dataset[:ntrain, ::2, :], x_dataset[ntrain:, ::2, :]
+y_trainset, y_testset = y_dataset[:ntrain, ::2, :], y_dataset[ntrain:, ::2, :]
+
+train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_trainset, y_trainset), batch_size=1, shuffle=True)
+test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_testset, y_testset), batch_size=1, shuffle=True)
+
+config = {
+    "model": {
+        "modes": [32], 
+        "fc_dim": 256, 
+        "act": "gelu", 
+        "num_pad": 4
+    },
+    "train": {
+        "epochs": 500,
+        "milestones": [50, 100],
+        "base_lr": 0.00005, 
+        "scheduler_gamma": 0.5
+    },
+}
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+in_dim, out_dim = x_trainset.shape[2], y_trainset.shape[2]
+print(in_dim, out_dim)
+
+model = FNO1d(modes=config['model']['modes'],
+              fc_dim=config['model']['fc_dim'],
+              act=config['model']['act'],
+              in_dim=in_dim, out_dim=out_dim).to(device)
+
+print(count_params(model))
+
+optimizer = torch.optim.Adam(model.parameters(), lr=config['train']['base_lr'], weight_decay=1e-4)
+scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=config['train']['milestones'], gamma=config['train']['scheduler_gamma'])
+
+rank = 0
+
+model.train()
+pbar_train = tqdm(range(config['train']['epochs']), dynamic_ncols=True, smoothing=0.1)
+
+loss_of_train, loss_of_test = [], []
+myloss = LpLoss(size_average=True)
+
+for e in pbar_train:
+    
+    model.train()
+    train_loss_l2 = 0.0
+
+    for x_train, y_train in train_loader:
+        x_train, y_train = x_train.to(rank), y_train.to(rank)
+
+        out_train = model(x_train)
+        train_data_loss = myloss(out_train, y_train)
+
+        optimizer.zero_grad()
+        train_data_loss.backward()
+        optimizer.step()
+
+        train_loss_l2 += train_data_loss.item()
+
+    scheduler.step()
+
+    train_loss_l2 /= len(train_loader)
+    loss_of_train.append(train_loss_l2)
+
+    # Evaluation on the test set
+    model.eval()
+    test_loss_l2 = 0.0
+
+    with torch.no_grad():
+        for x_test, y_test in test_loader:
+            x_test, y_test = x_test.to(rank), y_test.to(rank)
+
+            out_test = model(x_test)
+            test_data_loss = myloss(out_test, y_test)
+
+            test_loss_l2 += test_data_loss.item()
+            
+    test_loss_l2 /= len(test_loader)
+    loss_of_test.append(test_loss_l2)
+
+    pbar_train.set_description((f'Epoch {e}, train loss: {train_loss_l2:.5f} ,  test loss: {test_loss_l2:.5f}'))
+
+print('Completed!')
+
+
+import numpy as np
+
+from matplotlib import pyplot as plt
+import matplotlib
+import matplotlib.ticker as ptick
+matplotlib.rcParams['mathtext.fontset'] = 'stix'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+
+# loss plot
+
+plt.figure(figsize=(6, 5))
+plt.plot(loss_of_train, color="black")
+plt.plot(loss_of_test, color="red")
+plt.xlabel('iterations', fontsize = 16)
+plt.ylabel('Total Loss', fontsize = 16)
+plt.title('Total Loss', fontsize = 20)
+plt.xlim(0, len(loss_of_train))
+plt.grid(True)
+plt.savefig("")
+plt.savefig("./figures/loss.png", bbox_inches='tight', dpi=150)
+plt.show()
+
+# result plot
+
+x_plot = np.linspace(0, 20, 500)
+n = 610
+validate_x = x_dataset[n, ::2, :].reshape(1, 500, 9).to(device)
+validate_y = y_dataset[n, ::2, :].reshape(1, 500, 2).to(device)
+validate_out = model(validate_x)
+
+plt.figure(figsize=(15, 5))
+plt.plot(x_plot, validate_y[0, :, 0].detach().cpu().numpy(), color = "black", label = "true")
+plt.plot(x_plot, validate_out[0, :, 0].detach().cpu().numpy(), color = "red", label = "predict")
+plt.grid(True, which='major', linestyle='-')
+plt.grid(True, which='minor', linestyle='--')
+plt.xlim(0, 20)
+plt.legend()
+plt.savefig("./figures/results.png", bbox_inches='tight', dpi=150)
+plt.show()
---
+++ squid_log/run_model.sh
@@ -0,0 +1,18 @@
+#！/bin/bash
+#PBS -q SQUID-H
+#PBS --group=G15408
+#PBS -l elapstim_req=1:10:10
+#PBS -l cpunum_job=76
+#PBS -l gpunum_job=8
+#PBS -m eb
+#PBS -T openmpi
+#PBS -M chawitk@hku.hk
+#PBS -e $job.err
+#PBS -o $job.out
+#PBS -r y
+cd $PBS_O_WORKDIR
+module load BaseGPU/2022
+source activate base
+nvidia-smi
+python ../ShFNO.py >& ./ShFNO.txt
+## mpirun ${NQSV_MPIOPTS} -np 2 -npernode 2 python $job.py >& $job.txt
